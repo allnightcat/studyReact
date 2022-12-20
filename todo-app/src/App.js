63b67fe976a77d467e1eb124396a99ed3b93c5f7
allnightcat <allnightcat12@gmail.com>
2022-12-20 21:55:40 +0900

    불변성 유지 및 컴포넌트 성능 최적화 작업

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -4,50 +4,58 @@ import TodoTemplate from './components/TodoTemplate';
 import TodoList from './components/TodoList';
 import { useCallback, useState, useRef } from 'react';
 
+function createBulkTodos() {
+  const array = [];
+  for (let i = 1; i <= 2500; i++) {
+    array.push({
+      id: i,
+      text: `할 일 ${i}`,
+      checked: false,
+    });
+  }
+  return array;
+}
+
 const App = () => {
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      text: '리액트의 기초 알아보기',
-      checked: true,
-    },
-    {
-      id: 2,
-      text: '컴포넌트 스타일링 해보기',
+  const [todos, setTodos] = useState(createBulkTodos);
+
+  // const [todos, setTodos] = useState([
+  //   {
+  //     id: 1,
+  //     text: '리액트의 기초 알아보기',
+  //     checked: true,
+  //   },
+  //   {
+  //     id: 2,
+  //     text: '컴포넌트 스타일링 해보기',
+  //     checked: false,
+  //   },
+  //   {
+  //     id: 3,
+  //     text: '일정 관리 앱 만들어 보기',
+  //     checked: false,
+  //   },
+  // ]);
+  const nextId = useRef(2501);
+  const onInsert = useCallback((text) => {
+    const todo = {
+      id: nextId.current,
+      text: text,
       checked: false,
-    },
-    {
-      id: 3,
-      text: '일정 관리 앱 만들어 보기',
-      checked: false,
-    },
-  ]);
-  const nextId = useRef(4);
-  const onInsert = useCallback(
-    (text) => {
-      const todo = {
-        id: nextId.current,
-        text: text,
-        checked: false,
-      };
-      setTodos([...todos, todo]);
-      nextId.current += 1;
-    },
-    [todos],
-  );
-  const onRemove = useCallback(
-    (id) => {
-      setTodos(todos.filter((todo) => todo.id !== id));
-    },
-    [todos],
-  );
+    };
+    setTodos((todos) => [...todos, todo]);
+    nextId.current += 1;
+  }, []);
+  const onRemove = useCallback((id) => {
+    setTodos((todos) => todos.filter((todo) => todo.id !== id));
+  }, []);
   const onToggle = useCallback((id) => {
-    setTodos(
+    setTodos((todos) =>
       todos.map((item) =>
         item.id === id ? { ...item, checked: !item.checked } : item,
       ),
     );
-  });
+  }, []);
   return (
     <div>
       <TodoTemplate>
