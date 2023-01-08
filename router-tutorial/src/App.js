9806f11e7c55ab03d41cbd8fc692b4d65c4940f7
allnightcat <allnightcat12@gmail.com>
2023-01-08 15:24:40 +0900

    URL 파라미터 다루기

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import About from "./pages/About";
 import Home from "./pages/Home";
+import Profile from "./pages/Prpfile";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
+      <Route path="/profiles/:username" element={<Profile />} />
     </Routes>
   );
 }
