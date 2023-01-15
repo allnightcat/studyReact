51df48fde8300a6b36d415503bc5089ea3d1f6c7
allnightcat <allnightcat12@gmail.com>
2023-01-15 14:02:05 +0900

    news list, news item 컴포넌트 만들기

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,31 +1,12 @@
 import './App.css';
 import { useState } from 'react';
 import axios from 'axios';
+import NewsList from './components/NewsList';
 
 function App() {
-  const [data, setData] = useState(null);
-  const onClick = async () => {
-    try {
-      const respose = await axios.get(
-        'https://jsonplaceholder.typicode.com/todos/1',
-      );
-      setData(respose.data);
-    } catch (e) {
-      console.log(e);
-    }
-  };
   return (
     <div className="App">
-      <div>
-        <button onClick={onClick}>불러오기</button>
-      </div>
-      {data && (
-        <textarea
-          rows={7}
-          value={JSON.stringify(data, null, 2)}
-          readOnly={true}
-        />
-      )}
+      <NewsList />
     </div>
   );
 }
